2fb30de44e55fc84c5852f65fbbbc117b573b1ec
bapata <bapata@ymail.com>
2020-03-22 12:28:02 -0700

    groups

---
+++ group_validation.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+// g++ --std=c++2a <this-file>
+using namespace std;
+
+int main() {
+  map<string, vector<string> > node_group = {
+    { "group1", { "h1.example.com", "h2.example.com" } },
+    { "group2", { "h3.example.com", "h4.example.com" } },
+    { "group3", { "h5.example.com", "h6.example.com" } },
+  };
+  return 0;
+}
