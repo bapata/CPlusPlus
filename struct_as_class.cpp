58eba7f7046aee01b192255150577878da9d158b
bapata <bapata@ymail.com>
2020-05-29 21:24:23 -0700

    Update struct_as_class.cpp

--- struct_as_class.cpp
+++ struct_as_class.cpp
@@ -13,8 +13,23 @@ struct SNode {
     void setX(T myx) { x=myx; }
 };
 
+template<class T>
+struct CNode {
+  private:
+    T x;
+  public:
+    CNode() { x=0; }
+    CNode(T myx) { x=myx; }
+    ~CNode() { x=-1; }
+    T getX() { return x; }
+    void setX(T myx) { x=myx; }
+};
+
 
 int main(int argc,char **argv,char **envp) {
   SNode<int> *p = new SNode<int>(10);
+  SNode<int> *q = new SNode<int>(20);
+
   cout << p->getX() <<endl;
+  cout << q->getX() <<endl;
 }
