17972f478f0e9cf3e24c08b39a7de3cd512b62dc
bapata <bapata@ymail.com>
2023-08-05 06:50:32 -0700

    Create gcd_lcm.cpp

---
+++ gcd_lcm.cpp
@@ -0,0 +1,15 @@
+#include <numeric>
+#include <iostream>
+
+// g++ <this-file> -std=c++2a
+// ./a.out
+
+using namespace std;
+
+int main() {
+
+auto gcd = std::gcd(10,20);
+
+cout << gcd <<endl;
+
+}
